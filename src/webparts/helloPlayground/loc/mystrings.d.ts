682382b35b65e86eb3d97538cedcf3714572b987
Krste <krste@kryptomine.com.au>
2019-07-30 18:24:27 +0200

    intial commit

---
+++ src/webparts/helloPlayground/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IHelloPlaygroundWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'HelloPlaygroundWebPartStrings' {
+  const strings: IHelloPlaygroundWebPartStrings;
+  export = strings;
+}
